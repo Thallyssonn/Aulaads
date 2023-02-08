9e1b133d542141b17a541b2f250a0c50cfd48b6b
Thallyssonn <123607620+Thallyssonn@users.noreply.github.com>
2023-02-08 23:10:27 +0000

    primeiro código git

---
+++ aula01.py
@@ -0,0 +1,12 @@
+n1 = float(input("entre com n1"))
+n2 = float(input("entre com o valor n1"))
+n3 = float(input("entre com o valr de n1"))
+me = float(input("entre com me"))
+
+ma = (n1 + n2 * 2 + n3 * 3 + me)/7
+
+if (ma >= 9):
+    print ("maior ou igual a 9")
+
+if (ma<4):
+    print("menor que 4")
