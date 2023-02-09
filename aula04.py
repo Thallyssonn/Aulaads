7519070cc6b88ab88a1b03cd259f82bf812a90b0
Thallyssonn <123607620+Thallyssonn@users.noreply.github.com>
2023-02-09 00:11:20 +0000

    salvar

---
+++ aula04.py
@@ -0,0 +1,32 @@
+print ("xxxx")
+print ("x  X")
+print ("x  x")
+print ("x  x")
+print ("x  x")
+print ("x  x")
+
+
+
+aluno = input ("entre com nome: ")
+nota = input (" entre co oa nota")
+
+print ("aluno"(a)         nota
+
+
+
+
+print ("/n0 - fim /n - inclui /n 2 - altera /n 3")
+a = input("entre com o meu")
+if (a == "0"):
+    print ("fim"0)
+if (A == "1"):
+    print ("inclui")
+if (a == "2"):
+        print ("altera")
+if (a == "3")
+   print("exclui")
+if (a == ("4")
+   print ("consulta")
+
+    
+
